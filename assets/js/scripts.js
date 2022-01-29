a320d3224961c121ca81392a95fe6709a1a430da
Hongnodie <95200204+Hongnodie@users.noreply.github.com>
2022-01-29 17:15:00 +1030

    Predetermined coding frame

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,3 +3,29 @@ var searchbtn = document.querySelector(".searchbtn");
 var citynameinput = document.getElementById("cityname");
 var citynamelist =[];
 
+var cityname;
+let latitude;
+let longitude;
+
+// Add search button that save city nanme to local storage as Step1
+searchbtn.addEventListener("click", function(event){
+  event.preventDefault();
+  cityname = citynameinput.value;
+  localStorage.setItem("City", JSON.stringify(cityname));
+  getCurrentWeather(cityname);
+});
+
+// Then successful display of current weather Step2 triggers: 1-Search history button (Step3-1) and 2-future weather forecast card (Step3-2)
+function getCurrentWeather(paraC) {
+
+}
+
+// Pointer and Function of show forecast card Step 3-2
+function showforecast() {
+
+}
+
+// Append button to the history of each cityname input
+function attachhistorybtn() {
+
+}
