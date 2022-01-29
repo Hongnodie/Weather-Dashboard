6a45cccbea0501ef7ea3c124965e40a8d1f2d640
Hongnodie <95200204+Hongnodie@users.noreply.github.com>
2022-01-29 17:16:34 +1030

    Added display current weather section but uv index left unsettled

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -16,8 +16,47 @@ searchbtn.addEventListener("click", function(event){
 });
 
 // Then successful display of current weather Step2 triggers: 1-Search history button (Step3-1) and 2-future weather forecast card (Step3-2)
-function getCurrentWeather(paraC) {
+// Current date acquisition variables
+var today = moment().format("MM/DD/YYYY");
+var currentweather = document.getElementById("current-weather");
+const apikey = "7e0dfeaa5d115777441ab4aec1f884c9";
 
+function getCurrentWeather(paraC) {
+  let currentweatherapi = `http://api.openweathermap.org/data/2.5/weather?q=${paraC}&units=imperial&appid=${apikey}`;
+
+  fetch(currentweatherapi)
+      .then(function (response) {
+        if (response.ok) {
+          response.json().then(function (response) {
+            // For check purpose
+            console.log(response);
+            // Handler of valid (with response) and new (not repeat) search input
+            if (!citynamelist.includes(paraC)) {
+              citynamelist.push(paraC);
+              localStorage.setItem("Citynamelist", JSON.stringify(citynamelist));
+              // Step3-1
+              attachhistorybtn();
+            }           
+            // Show only some index
+            currentweather.innerHTML = `
+              <h3>City of ${response.name}  ${today} </h3>
+              <img src="https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png" class="iconimage">
+              Summary: ${response.weather[0].description}<br>
+              Temperature: ${response.main.temp} °F <br>
+              Humidity: ${response.main.humidity} % <br>
+              Wind Speed: ${response.wind.speed} mph <br>`
+            // Pass latitude and longitude to forecast board
+            latitude = response.coord.lat;
+            longitude = response.coord.lon;
+            // Step3-2
+            showforecast();
+          })
+        } 
+        else { 
+          alert('Error: ' + response.statusText); 
+        }
+      }).catch(function (error) { alert('Invalid function therefore unable to retrieve weather data'); }
+    );
 }
 
 // Pointer and Function of show forecast card Step 3-2
